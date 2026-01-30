4bdd646088cb757f7b29881311c272d92a752711
Maria Smith <maria.t.smith023@gmail.com>
2026-01-29 23:16:52 -0500

    rename files

--- popup/popup.jsx
+++ popup/popup.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+function Popup() {
+  return (
+    <div>
+      <h1>Clipboard Manager</h1>
+      <p>Clips will go here!</p>
+    </div>
+  );
+}
+
+export default Popup;
