2cbc591ea983bd8ae186747697e4618cbcb878ef
Maria Smith <maria.t.smith023@gmail.com>
2026-01-29 21:14:18 -0500

    setup react

---
+++ popup/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Clipboard Manager</h1>
+      <p>Clips will go here!</p>
+    </div>
+  );
+}
+
+export default App;
