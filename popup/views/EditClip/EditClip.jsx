f4a4847d89f9965332fac66d859e32fde507c89a
Maria Smith <maria.t.smith023@gmail.com>
2026-05-17 14:35:18 -0400

    edit clip input styling

--- popup/views/EditClip/EditClip.jsx
+++ popup/views/EditClip/EditClip.jsx
@@ -16,6 +16,23 @@ const EditClip = ({ isCreate }) => {
           <h1>{isCreate ? 'Create' : 'Edit'} Clip</h1>
         </div>
       </div>
+      <div className='input-container'>
+        <label>Clip</label>
+        <textarea 
+          className='clip-textarea' 
+          placeholder='Clip' 
+          rows={1}
+          onChange={(e) => {
+            e.target.style.height = 'auto';
+            e.target.style.height = e.target.scrollHeight + 'px';
+            e.target.style.overflow = e.target.scrollHeight >= 400 ? 'auto' : 'hidden';
+          }}
+        />
+      </div>
+      <div className='edit-actions'>
+        <button>Cancel</button>
+        <button>Save</button>
+      </div>
     </>
   )
 }
